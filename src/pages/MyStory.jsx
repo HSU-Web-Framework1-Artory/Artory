17121acf8b35dd7c649420b850146fe6f117962a
Juye0nLee <dlwndus708@naver.com>
2024-10-31 15:22:06 +0900

    💄 캘린더 디테일 수정

--- src/pages/MyStory.jsx
+++ src/pages/MyStory.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components'
-import Calendar from '../entites/MyStory/ui/Calendar'
+import moment from 'moment'
 import ProfileSection from '../entites/MyStory/ui/ProfileSection'
 import CalendarAndMemo from '../entites/MyStory/ui/CalendarAndMemo'
 import SavedExhibitionList from '../entites/MyStory/ui/SavedExhibitionList'
 
 export default function MyStory() {
+  const [currentYear, setCurrentYear] = useState(moment().year());
   return (
     <MainLayout>
 
         {/* 프로필과 버튼 섹션 */}
-        <ProfileSection/>
+        <ProfileSection currentYear={currentYear}/>
 
         {/* 메모와 캘린더 섹션 */}
-        <CalendarAndMemo/>
+        <CalendarAndMemo currentYear={currentYear} setCurrentYear={setCurrentYear}/>
 
         {/* 저장한 전시 리스트 */}
         <SavedExhibitionList/>
