76997102166a040da34de880600791b265b6391a
Juye0nLee <dlwndus708@naver.com>
2024-10-31 17:22:00 +0900

    ✨ StandardInput 구현

--- src/entites/MyStory/ui/Modal.jsx
+++ src/entites/MyStory/ui/Modal.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import ReactModal from 'react-modal'
+import StandardInput from '../../../shared/components/StandardInput';
+
+//이미지
+import GLASSES from '../../../assets/searchicon.svg'
+
 export default function Modal({isShowModal, setIsShowModal}) {
+    const [isInputClick, setIsInputClick] = useState(false);
   return (
         <ReactModal 
             isOpen={isShowModal}
             onRequestClose={() => setIsShowModal(false)}
             style={StyledModal}
         >
-
+            <StandardInput 
+                placeholder="전시 검색" 
+                width="34.89513rem"
+                height="3.69913rem"
+                marginTop="0"
+                isInputClick={isInputClick} 
+                onFocus={()=>setIsInputClick(true)}
+                onBlur={()=>setIsInputClick(false)}
+            />
+            <StyledImg src={GLASSES}/>
         </ReactModal>
   )
 }
@@ -29,7 +44,15 @@ const StyledModal = ReactModal.Styles = {
       left: "50%",
       transform: "translate(-50%, -50%)",
       backgroundColor: "#fff",
-      justifyContent: "center",
+      display: "flex",
+      flexDirection : "column",
+      alignItems: "center",
       overflow: "auto",
     },
   };
+
+const StyledImg = styled.img`
+width: 3.75rem;
+height: 3.75rem;
+margin-top : 8rem;
+`
